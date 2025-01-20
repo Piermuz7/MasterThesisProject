5b8cb49f08e7ae2d2e5a1da1054fe7f9ab5861d5
Piermuz7 <piermichele.rosati@gmail.com>
2025-01-21 00:57:25 +0100

    [edit] fixed warning for LLM instances

--- rag/llm.py
+++ rag/llm.py
@@ -1,35 +1,40 @@
 import streamlit as st
 from langchain_anthropic import ChatAnthropic
+from langchain_core.language_models import BaseLanguageModel
 from langchain_ollama.llms import OllamaLLM
 
 # Large Language Model configuration
 
 # Anthropic configuration
-'''llm = ChatAnthropic(temperature=0,
+'''llms = ChatAnthropic(temperature=0,
                     anthropic_api_key=st.secrets["ANTHROPIC_KEY"],
                     model="claude-3-5-sonnet-20240620")
 '''
 
 # Ollama configuration
-'''llm = OllamaLLM(
+'''llms = OllamaLLM(
     temperature=0,
     model="llama3.2",
     base_url="http://localhost:11434"
 )
 '''
 '''
-llm = OllamaLLM(
+llms = OllamaLLM(
     temperature=0,
     model="llama3.1",
     base_url="http://localhost:11434"
 )
 '''
 import toml
-from llm.llm_factory import LLMFactory
+from rag.llms.llm_factory import LLMFactory
 
-config = toml.load("../.streamlit/secrets.toml")
+config = toml.load(".streamlit/secrets.toml")
 
 llm_type = config["llm"]["PROVIDER"]
 api_key = config["llm"].get("api_key", None)
 
 llm = LLMFactory.get_llm(llm_type, api_key)
+
+
+def get_llm() -> BaseLanguageModel:
+    return llm
--- rag/llms/llm_factory.py
+++ rag/llms/llm_factory.py
@@ -1,11 +1,11 @@
 from langchain_core.language_models import BaseLanguageModel
-from rag.llm.claude import ClaudeLLM
-from rag.llm.llama import LlamaLLM
+from rag.llms.claude import ClaudeLLM
+from rag.llms.llama import LlamaLLM
 
 
 class LLMFactory:
     @staticmethod
-    def get_llm(model_type: str, api_key: str = None) -> BaseLanguageModel:
+    def get_llm(model_type: str, api_key = None) -> BaseLanguageModel:
         """Factory method to return the appropriate LLM instance."""
         if model_type == "claude":
             return ClaudeLLM(api_key)
