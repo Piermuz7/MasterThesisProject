bd9d21619e27ec1f236ce9598295032d9249f90d
Piermuz7 <piermichele.rosati@gmail.com>
2025-02-07 23:29:01 +0100

    [edit] removed agent.py

--- rag/llm.py
+++ rag/llm.py
@@ -8,38 +8,12 @@ from rag.llms.llama_index_llm.llm_factory import LLMFactory as LLMFactoryLI
 
 # Large Language Model configuration
 
-'''
-llama_index_llm = Anthropic(
-    model="claude-3-5-sonnet-20241022",
-    api_key=st.secrets["api_key"]["ANTHROPIC_KEY"],
-    max_tokens=8192,
-)
-'''
-
-# Anthropic configuration
-'''llms = ChatAnthropic(temperature=0,
-                    anthropic_api_key=st.secrets["ANTHROPIC_KEY"],
-                    model="claude-3-5-sonnet-20240620")
-'''
-
-# Ollama configuration
-'''llms = OllamaLLM(
-    temperature=0,
-    model="llama3.2",
-    base_url="http://localhost:11434"
-)
-'''
-'''
-llms = OllamaLLM(
-    temperature=0,
-    model="llama3.1",
-    base_url="http://localhost:11434"
-)
-'''
-
 config = toml.load(".streamlit/secrets.toml")
 
 llm_type = config["llm"]["PROVIDER"]
+
+# decomment the API key that you want to use
+
 # api_key = config["api_key"]
 api_key = config["api_key"]["ANTHROPIC_KEY"]
 
